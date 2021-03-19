fc4aaf535753a669134f2893fdadb2872d1360f3
Sidelkovsky <sidelkovsky@tuta.io>
2021-03-19 04:55:20 +0100

    Templates Introduced.

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-	return "Hello, World!"
+	user = {'username':'Kirill'}
+	posts = [
+		{
+			'author': {'username': 'John'},
+			'body': 'Beautiful day in Portland!'
+		},
+		{
+			'author': {'username': 'Susan'},
+			'body': 'The Avengers Move was So Cool!'
+		},
+	]
+	return render_template('index.html', title='Home', user=user, posts=posts)
